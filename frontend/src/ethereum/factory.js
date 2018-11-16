5bc8abd1cadcd7a744a22d3d297677c4b844b030
diggzhang <diggzhang@gmail.com>
2018-11-17 01:26:36 +0800

    最新合约地址

--- frontend/src/ethereum/factory.js
+++ frontend/src/ethereum/factory.js
@@ -3,7 +3,7 @@ import compiledCampaignFactory from './build/CampaignFactory.json';
 
 const campaignFactory = new web3.eth.Contract(
   JSON.parse(compiledCampaignFactory.interface),
-  '0x728D0B8E573aAd130109BdC2Ef39384eCBBfd450'
+  '0xfA2266d60a5B555cc3B5CeF8da05CDbb9AB5FA92'
 );
 
 export default campaignFactory;
